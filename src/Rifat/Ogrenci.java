9104a15832a3ce2b93f036247e3cb7084950680a
rftbtr <rifatbatir@gmail.com>
2025-03-05 12:37:08 +0300

    Öğrenci class'ı oluşturuldu

---
+++ src/Rifat/Ogrenci.java
@@ -0,0 +1,18 @@
+package Rifat;
+
+public class Ogrenci {
+    public String okulNo;
+    public String ad;
+    public String soyad;
+    public String bolum;
+    public String sinif;
+    public String cinsiyet;
+    public String dogumTarihi;
+    public String dogumYeri;
+    public String telefon;
+    public String email;
+    public String adres;
+
+
+    }
+}
